206b14fdd699e08dc35aad45c9cb4d303864e6ae
Abhijeet Kumar <25275343+itsabhijeet@users.noreply.github.com>
2018-06-26 10:49:31 +0530

    Privacy

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 app.secret_key = 'abhiportfolio'
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'handoverabhi@gmail.com'
-app.config['MAIL_PASSWORD'] = 'handover'
+app.config['MAIL_USERNAME'] = '' #email
+app.config['MAIL_PASSWORD'] =  #password
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
