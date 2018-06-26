b0d2316208e4442c50013879be1f117b8c40a14c
abhilearner <abhijeetown540@outlook.com>
2018-06-26 10:47:53 +0530

    Complete MVP

--- main.py
+++ main.py
@@ -5,11 +5,20 @@ from flask_mail import Mail,Message
 
 app = Flask(__name__)
 app.secret_key = 'abhiportfolio'
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'handoverabhi@gmail.com'
+app.config['MAIL_PASSWORD'] = 'handover'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
 
 class msgform(Form):
       name = TextField('Name')
       email=TextField('Email')
       msg = TextAreaField('Leave Message')
+class EmailForm(Form):
+    email= TextField("Email",[validators.Required("Recepients Email id"),validators.Email("Enter Email")])      
 
 @app.route('/')
 def home():
@@ -31,20 +40,37 @@ def msg():
          print (email)
          print(msg)
          if form.validate():
-            #Sending an email to myself
-            app.config['MAIL_SERVER']='smtp.gmail.com'
-            app.config['MAIL_PORT'] = 465
-            app.config['MAIL_USERNAME'] = 'handoverabhi@gmail.com'
-            app.config['MAIL_PASSWORD'] = 'handover'
-            app.config['MAIL_USE_TLS'] = False
-            app.config['MAIL_USE_SSL'] = True
+            #Sending an email to myself           
             mail = Mail(app)            
             newmsg = Message('!Sarahah', sender = 'handoverabhi@gmail.com', recipients = ['abhijeetown540@outlook.com'])
             newmsg.html = "<p> Name : " + name + "</p> <br> <p> Email :" + email  + "</p>" + "<br> Message: <p> <b> <i> " + msg + "</b> </i> </p>"
             mail.send(newmsg)
+            return redirect(url_for('takeemail'))
          else:
              flash('Email Not correct bro!') 
     return render_template('contact.html',form=form)
+
+@app.route('/takeemail',methods=['GET','POST'])
+def takeemail():
+     form = EmailForm(request.form)
+     
+     if request.method == 'POST':
+         email=request.form['email']
+         print (email)
+         if form.validate():
+             mail= Mail(app)
+             Nmsg = Message('Not Sarahah',sender='handoverabhi@gmail.com',recipients=[email])
+             Nmsg.html  = " <b> Thanks for your interest. We are working for you! </b> <br> <i> Abhijeet </i> "
+             mail.send(Nmsg)
+             Nmsg = Message('People Interested',sender='hanoverabhi@gmail.com',recipients=['abhijeetown540@outlook.com'])
+             Nmsg.body = email + "\nHe/she seems to be Interested."
+             mail.send(Nmsg)
+             return render_template('email.html')
+         else:
+             flash('Email Not correct bro!') 
+     return render_template('takeemail.html',form=form)
+
+
 
     
 
